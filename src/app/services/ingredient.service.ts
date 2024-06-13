eac0a49be486afd3605075a4204f7ca13c1c865a
U-DESKTOP-QKC9GKG\Clément <clement.gournay-ext@epicea.eu>
2024-06-13 08:22:41 +0200

    Updated dist

--- src/app/services/ingredient.service.ts
+++ src/app/services/ingredient.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, lastValueFrom, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 
 @Injectable({
